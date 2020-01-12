4f033b70c9ebc7c6ac09042782dc55848ea283e4
al Mohamady <al.Mohamady47@gmail.com>
2020-01-12 16:37:42 +0200

    ADD BLOC PATTERN

---
+++ lib/bloc/provider.dart
@@ -0,0 +1,17 @@
+import 'package:the_food_cart_app/model/foodItem.dart';
+
+
+class CartProvider {
+
+  List<FoodItem> foodItems = [];
+
+  List<FoodItem> addToList(FoodItem foodItem) {
+    foodItems.add(foodItem);
+    return foodItems;
+  }
+
+  List<FoodItem> removeFromList(FoodItem foodItem) {
+    foodItems.remove(foodItem);
+    return foodItems;
+  }
+}
